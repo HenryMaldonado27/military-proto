1440694ced96685e8dbf50a686bfdca8a0625e24
HenryMaldonado27 <henry.rams9@gmail.com>
2026-04-30 01:37:42 -0600

    protos

--- proto/military.pb.go
+++ proto/military.pb.go
@@ -22,11 +22,13 @@ const (
 )
 
 type MilitaryEvent struct {
-	state         protoimpl.MessageState `protogen:"open.v1"`
-	Id            string                 `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
-	Type          string                 `protobuf:"bytes,2,opt,name=type,proto3" json:"type,omitempty"`
-	unknownFields protoimpl.UnknownFields
-	sizeCache     protoimpl.SizeCache
+	state           protoimpl.MessageState `protogen:"open.v1"`
+	Country         string                 `protobuf:"bytes,1,opt,name=country,proto3" json:"country,omitempty"`
+	WarplanesInAir  int32                  `protobuf:"varint,2,opt,name=warplanes_in_air,json=warplanesInAir,proto3" json:"warplanes_in_air,omitempty"`
+	WarshipsInWater int32                  `protobuf:"varint,3,opt,name=warships_in_water,json=warshipsInWater,proto3" json:"warships_in_water,omitempty"`
+	Timestamp       int64                  `protobuf:"varint,4,opt,name=timestamp,proto3" json:"timestamp,omitempty"`
+	unknownFields   protoimpl.UnknownFields
+	sizeCache       protoimpl.SizeCache
 }
 
 func (x *MilitaryEvent) Reset() {
@@ -59,76 +61,44 @@ func (*MilitaryEvent) Descriptor() ([]byte, []int) {
 	return file_military_proto_rawDescGZIP(), []int{0}
 }
 
-func (x *MilitaryEvent) GetId() string {
+func (x *MilitaryEvent) GetCountry() string {
 	if x != nil {
-		return x.Id
+		return x.Country
 	}
 	return ""
 }
 
-func (x *MilitaryEvent) GetType() string {
+func (x *MilitaryEvent) GetWarplanesInAir() int32 {
 	if x != nil {
-		return x.Type
+		return x.WarplanesInAir
 	}
-	return ""
+	return 0
 }
 
-type Ack struct {
-	state         protoimpl.MessageState `protogen:"open.v1"`
-	Status        string                 `protobuf:"bytes,1,opt,name=status,proto3" json:"status,omitempty"`
-	unknownFields protoimpl.UnknownFields
-	sizeCache     protoimpl.SizeCache
-}
-
-func (x *Ack) Reset() {
-	*x = Ack{}
-	mi := &file_military_proto_msgTypes[1]
-	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-	ms.StoreMessageInfo(mi)
-}
-
-func (x *Ack) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*Ack) ProtoMessage() {}
-
-func (x *Ack) ProtoReflect() protoreflect.Message {
-	mi := &file_military_proto_msgTypes[1]
+func (x *MilitaryEvent) GetWarshipsInWater() int32 {
 	if x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
+		return x.WarshipsInWater
 	}
-	return mi.MessageOf(x)
+	return 0
 }
 
-// Deprecated: Use Ack.ProtoReflect.Descriptor instead.
-func (*Ack) Descriptor() ([]byte, []int) {
-	return file_military_proto_rawDescGZIP(), []int{1}
-}
-
-func (x *Ack) GetStatus() string {
+func (x *MilitaryEvent) GetTimestamp() int64 {
 	if x != nil {
-		return x.Status
+		return x.Timestamp
 	}
-	return ""
+	return 0
 }
 
 var File_military_proto protoreflect.FileDescriptor
 
 const file_military_proto_rawDesc = "" +
 	"\n" +
-	"\x0emilitary.proto\x12\bmilitary\"3\n" +
-	"\rMilitaryEvent\x12\x0e\n" +
-	"\x02id\x18\x01 \x01(\tR\x02id\x12\x12\n" +
-	"\x04type\x18\x02 \x01(\tR\x04type\"\x1d\n" +
-	"\x03Ack\x12\x16\n" +
-	"\x06status\x18\x01 \x01(\tR\x06status2I\n" +
-	"\x0fMilitaryService\x126\n" +
-	"\fPublishEvent\x12\x17.military.MilitaryEvent\x1a\r.military.AckB\x16Z\x14military-proto/protob\x06proto3"
+	"\x0emilitary.proto\x12\x05proto\"\x9d\x01\n" +
+	"\rMilitaryEvent\x12\x18\n" +
+	"\acountry\x18\x01 \x01(\tR\acountry\x12(\n" +
+	"\x10warplanes_in_air\x18\x02 \x01(\x05R\x0ewarplanesInAir\x12*\n" +
+	"\x11warships_in_water\x18\x03 \x01(\x05R\x0fwarshipsInWater\x12\x1c\n" +
+	"\ttimestamp\x18\x04 \x01(\x03R\ttimestampB2Z0github.com/HenryMaldonado27/military-proto/protob\x06proto3"
 
 var (
 	file_military_proto_rawDescOnce sync.Once
@@ -142,16 +112,13 @@ func file_military_proto_rawDescGZIP() []byte {
 	return file_military_proto_rawDescData
 }
 
-var file_military_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_military_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
 var file_military_proto_goTypes = []any{
-	(*MilitaryEvent)(nil), // 0: military.MilitaryEvent
-	(*Ack)(nil),           // 1: military.Ack
+	(*MilitaryEvent)(nil), // 0: proto.MilitaryEvent
 }
 var file_military_proto_depIdxs = []int32{
-	0, // 0: military.MilitaryService.PublishEvent:input_type -> military.MilitaryEvent
-	1, // 1: military.MilitaryService.PublishEvent:output_type -> military.Ack
-	1, // [1:2] is the sub-list for method output_type
-	0, // [0:1] is the sub-list for method input_type
+	0, // [0:0] is the sub-list for method output_type
+	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -168,9 +135,9 @@ func file_military_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_military_proto_rawDesc), len(file_military_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   1,
 			NumExtensions: 0,
-			NumServices:   1,
+			NumServices:   0,
 		},
 		GoTypes:           file_military_proto_goTypes,
 		DependencyIndexes: file_military_proto_depIdxs,
